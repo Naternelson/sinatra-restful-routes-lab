0eb71e24efe593949d6431db0dbada24561660a2
Nate <7nater.nelson@gmail.com>
2021-02-27 17:00:47 -0700

    commit tree 748ae60cb3a182ab8ea723f86fb0bb61a011a13c

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,59 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  # code actions here!
+  
+  get '/recipes' do 
+    @recipes = Recipe.all
+    erb :index
+  end
 
+  get '/recipes/new' do
+    erb :new
+  end
+
+  get '/recipes/:id' do
+    @recipe = Recipe.find_by(id: params[:id])
+    if @recipe
+      erb :show
+    else
+      redirect '/recipes'
+    end
+  end
+
+
+  get '/recipes/:id/edit' do
+    @recipe = Recipe.find_by(id: params[:id])
+    if @recipe
+      erb :edit
+    else
+      redirect '/recipes'
+    end
+  end
+
+  post '/recipes' do 
+    @recipe = Recipe.create(
+      name: params[:name],
+      ingredients: params[:ingredients],
+      cook_time: params[:cook_time].to_f
+    )
+    redirect "/recipes/#{@recipe.id}"
+  end
+
+  patch '/recipes/:id' do
+    @recipe = Recipe.find_by(id: params[:id])
+    if @recipe
+      @recipe.update(name: params[:name], ingredients: params[:ingredients], cook_time: params[:cook_time])
+      redirect "/recipes/#{@recipe.id}"
+    else
+      redirect '/recipes'
+    end
+  end
+
+  delete '/recipes/:id' do
+    @recipe = Recipe.find_by(id: params[:id])
+    if @recipe
+      @recipe.delete
+    end
+    redirect '/recipes'
+  end
 end
